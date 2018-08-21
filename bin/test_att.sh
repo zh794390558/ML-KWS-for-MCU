da57098de1a3e481350c90c6656c785d2a723619
Hui Zhangh <zhtclz@foxmail.com>
2018-08-21 16:16:33 +0800

    fix tran scripts

--- bin/test_att.sh
+++ bin/test_att.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-python test.py --data_url '' --data_dir /nfs/project/zhanghui/data/kws/opensource \
+
+CUDA_VISIBLE_DEVICES=0 python test.py --data_url '' --data_dir /nfs/project/zhanghui/data/kws/opensource \
  --model_architecture attention \
  --model_size_info 98 144 70 --dct_coefficient_count 10 \
- --window_size_ms 40 --window_stride_ms 40 \
- --checkpoint work/ATTENTION/ATTENTION1/training/best/attention_9198.ckpt-20000
+ --window_size_ms 100 --window_stride_ms 100 \
+ --checkpoint work/ATTENTION/ATTENTION2/training/best/attention_9020.ckpt-16800 &> work/ATTENTION/ATTENTION2/test.log 
--- bin/test_rmn.sh
+++ bin/test_rmn.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-python test.py --data_dir '' --data_dir /nfs/project/zhanghui/data/kws/opensource \
+
+CUDA_VISIBLE_DEVICES=0 python test.py --data_url '' --data_dir /nfs/project/zhanghui/data/kws/opensource \
  --model_architecture rmn \
- --model_size_info 98 18 True 3 144 70 \
+ --model_size_info 98 18 144 1 3 70 \
  --dct_coefficient_count 10 --window_size_ms 40 --window_stride_ms 40 \
- --checkpoint work/RMN/RMN1/training/best/rmn_9175.ckpt-11600
+ --checkpoint work/RMN/RMN2/training/best/rmn_9403.ckpt-21600  &> work/RMN/RMN2/test.log
